189bf1b08d8b722d3344ece7f1dfebc83ef7ecaf
hmadafzal <hmadafzal00@gmail.com>
2026-04-25 09:12:51 +0500

    init

---
+++ main.py
@@ -0,0 +1,37 @@
+from rag import rag_pipeline
+from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+uploaded_file = None
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class QueryRequest(BaseModel):
+	question:str
+
+@app.post('/upload')
+async def upload(file: UploadFile = File(...)):
+    global uploaded_file
+    contents = await file.read()
+    with open(file.filename, "wb") as f:
+        f.write(contents)
+    uploaded_file = file.filename
+    return {"filename": file.filename, "message": "PDF uploaded successfully"}
+
+@app.post('/query')
+def query(request:QueryRequest):
+	question=request.question
+	if uploaded_file == None:
+		raise HTTPException(status_code=400, detail="No PDF uploaded yet")
+	try:
+		answer=rag_pipeline(uploaded_file, question)
+		return {"answer": answer}
+	except Exception as e:
+		raise HTTPException(status_code=500, detail="Error in generating answer")
---
+++ rag.py
@@ -0,0 +1,92 @@
+from pypdf import PdfReader
+import re
+import numpy as np
+import faiss
+from transformers import pipeline
+from sentence_transformers import SentenceTransformer
+
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+llm = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+
+
+def load_pdf(path):
+  reader = PdfReader(path)
+  text=""
+  for page in reader.pages:
+    text+=page.extract_text()
+  return text
+
+def chunk_text(text, chunk_size=500, overlap_sentences=2):
+    sentences = re.split(r'(?<=[.!?])\s+', text)
+    chunks = []
+    current_chunk = ""
+    current_sentences = []
+
+    for sentence in sentences:
+        if len(current_chunk) + len(sentence) <= chunk_size:
+            current_chunk += sentence + " "
+            current_sentences.append(sentence)
+        else:
+            if current_chunk:
+                chunks.append(current_chunk.strip())
+            overlap = current_sentences[-overlap_sentences:]
+            current_chunk = " ".join(overlap) + " " + sentence + " "
+            current_sentences = overlap + [sentence]
+
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+
+    return chunks
+
+
+def embed_chunks(chunks):
+  embeded_chunks=model.encode(chunks)
+  embeded_chunks=np.array(embeded_chunks, dtype="float32")
+  return embeded_chunks
+
+def build_index(embeddings):
+    index = faiss.IndexFlatL2(embeddings.shape[1])
+    index.add(embeddings)
+    return index
+
+
+def FindIndex(index, question, k):
+  embeded_chunk=embed_chunks(question)
+  embeded_chunk=embeded_chunk.reshape(1,-1)
+  D,I=index.search(embeded_chunk, k)
+  return D,I
+
+
+def generate_answer(question, chunks, retrieved_indices):
+    context = ""
+    for i in retrieved_indices[0]:
+        context += chunks[i] + "\n\n"
+
+    prompt = f"""<|system|>
+You are an expert assistant that answers questions strictly based on the provided context.
+Follow these rules:
+- If the answer is in the context, answer clearly and concisely in 2-3 sentences.
+- If the answer is NOT in the context, say exactly "I don't have enough information to answer this question."
+- Never make up information.
+- Always be specific and precise.</s>
+<|user|>
+Context:
+{context}
+
+Question: {question}</s>
+<|assistant|>"""
+
+    output = llm(prompt, max_new_tokens=400, do_sample=False)
+    answer = output[0]['generated_text'].split("<|assistant|>")[-1].strip()
+    return answer
+
+
+def rag_pipeline(pdf_path, question, k=3):
+    text = load_pdf(pdf_path)
+    chunks = chunk_text(text)
+    embeddings = embed_chunks(chunks)
+    index = build_index(embeddings)
+    D,I = FindIndex(index, question, k)
+    answer = generate_answer(question, chunks, I)
+    return answer
