634fbdc235c73176d33f3a0a49bfe7a421bdfbf9
hmadafzal <hmadafzal00@gmail.com>
2026-04-25 22:12:50 +0500

    Multi PDF support added

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from rag import rag_pipeline
 from pydantic import BaseModel
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 app = FastAPI()
-uploaded_file = None
+uploaded_files = []
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,29 +16,33 @@ app.add_middleware(
 
 class QueryRequest(BaseModel):
     question: str
+    filename: str
 
 @app.get("/")
 def read_root():
     return FileResponse("index.html")
 
+@app.get('/files')
+def get_files():
+    return {"files": uploaded_files}
+
 @app.post('/upload')
 async def upload(file: UploadFile = File(...)):
-    global uploaded_file
+    global uploaded_files
     if not file.filename.endswith('.pdf'):
-    	raise HTTPException(status_code=400, detail="Only PDFs are supported")
+        raise HTTPException(status_code=400, detail="Only PDFs are supported")
     contents = await file.read()
     with open(file.filename, "wb") as f:
         f.write(contents)
-    uploaded_file = file.filename
+    uploaded_files.append(file.filename)
     return {"filename": file.filename, "message": "PDF uploaded successfully"}
 
 @app.post('/query')
 def query(request: QueryRequest):
-    question = request.question
-    if uploaded_file == None:
+    if len(uploaded_files) == 0:
         raise HTTPException(status_code=400, detail="No PDF uploaded yet")
     try:
-        answer = rag_pipeline(uploaded_file, question)
+        answer = rag_pipeline(request.filename, request.question)
         return {"answer": answer}
     except Exception as e:
         print(f"ERROR: {e}")
